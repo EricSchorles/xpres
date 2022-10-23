720a4a9f86b4036b0b75768c702a44c070f28ac4
Eric William Schorles Júnior <eric.junior@globalsys.com.br>
2022-10-23 20:53:14 -0300

    add types

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 const path = require('path');
 
 const port = process.env.PORT || 3000;
+app.get("/ola", function (req, res) { res.send("ola"); })
 
-app.get("/ola", function (req,res) { res.send("ola");} )
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname + 'src/index.html'));
+  //__dirname : It will resolve to your project folder.
+});
 
-app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/index.html'));
-    //__dirname : It will resolve to your project folder.
-  });
-
-app.listen(port, () => {console.log(port, "local")} )
+app.listen(port, () => { console.log(port, "local") })
