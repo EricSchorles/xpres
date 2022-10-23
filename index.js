741a4783f42d7477e13c09268e70f96740379086
Eric William Schorles Júnior <eric.junior@globalsys.com.br>
2022-10-23 19:54:18 -0300

    try to add google ads

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const app = express();
+const path = require('path');
 
 const port = process.env.PORT || 3000;
 
-app.get("/", function (req,res) { res.send("ola");} )
+app.get("/ola", function (req,res) { res.send("ola");} )
+
+app.get('/',function(req,res){
+    res.sendFile(path.join(__dirname+'/index.html'));
+    //__dirname : It will resolve to your project folder.
+  });
 
 app.listen(port, () => {console.log(port, "local")} )
