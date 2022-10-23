f8cce1d0d3bb66ab723864310accc630f9ec8854
Eric William Schorles Júnior <eric.junior@globalsys.com.br>
2022-10-23 20:58:19 -0300

    fix path to index html

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3000;
 app.get("/ola", function (req, res) { res.send("ola"); })
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname + 'src/index.html'));
+  res.sendFile(path.join(__dirname.concat('/src') + '/index.html'));
   //__dirname : It will resolve to your project folder.
 });
 
